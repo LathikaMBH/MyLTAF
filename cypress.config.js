00d7416bda70bda781a005973a6b4c4d5abe02b6
LathikaMBH <lathika.mbh@gmail.com>
2024-11-12 22:07:28 +0530

    added base url to cypress.config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    url: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/support/page-objects/base-page.ts
+++ cypress/support/page-objects/base-page.ts
@@ -8,9 +8,10 @@ export class BasePage {
      * from this method user can visit to the any webpage.
      */
     public visit_base_page(): OranegHrmLoginPage {
+        let URL = Cypress.env('url');
          //alert(Cypress.env("URL"));
        // cy.visit(Cypress.env('URL'));
-       cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+       cy.visit(URL);
        //cy.visit(Cypress.env("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"));
         return new OranegHrmLoginPage();
     }
