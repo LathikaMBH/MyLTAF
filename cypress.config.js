b8f16f09cfea3970301e37542efa8aedb8b49773
LathikaMBH <lathika.mbh@gmail.com>
2024-11-15 09:55:37 +0530

    added the mochawesom reporting plugin

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   env: {
     url: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
   },
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
       config.env.URL = process.env.URL;
       return config;
