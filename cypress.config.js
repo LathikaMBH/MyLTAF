83814fc30d43bfd9b7d15b76ec82f32a2b591916
LathikaMBH <lathika.mbh@gmail.com>
2024-11-15 12:27:42 +0530

    remove mochasowe plugin

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  //reporter: 'cypress-mochawesome-reporter',
   env: {
     url: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
   },
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/register')(on);
+      //require('cypress-mochawesome-reporter/register')(on);
       // implement node event listeners here
       config.env.URL = process.env.URL;
       return config;
